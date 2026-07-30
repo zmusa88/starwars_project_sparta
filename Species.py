2d030a2de3e5f577c4f6901246b5132cd81558aa
rokii808 <debbie9taiwo@gmail.com>
2026-07-30 11:41:11 +0100

    Character

---
+++ Species.py
@@ -0,0 +1,13 @@
+class Species:
+
+    def __init__(self, name, classification, designation, average_height, skin_color,hair_color, eye_color, average_lifespan, language ):
+        self._name = name
+        self._classification = classification
+        self._designation = designation
+        self._average_height = average_height
+        self._skin_color = skin_color
+        self._hair_color = hair_color
+        self._eye_color = eye_color
+        self._average_lifespan = average_lifespan
+        self._language = language
+
